4fcb8e9b43c73deb9bf4d656c90bbbcba6c80907
yogesh9950 <yogeshsaini6367@gmail.com>
2026-06-01 17:56:45 +0530

    fix cors

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: '5mb' }));
 
 // ==============================
